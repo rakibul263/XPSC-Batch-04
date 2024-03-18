2bc5e917a96d484b726d3d3847b3ad4c022214fd
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-03-18 16:49:39 +0600

    cp

---
+++ Week-01/Day-04/C_Word_Game.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        map<string, int> mp;
+        vector<string> a[3];
+        for (int i = 0; i < 3; i++)
+        {
+            a[i].resize(n);
+            for (int j = 0; j < n; j++)
+            {
+                cin >> a[i][j];
+                mp[a[i][j]]++;
+            }
+        }
+        int p[3] = {0};
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (mp[a[i][j]] == 1)
+                    p[i] += 3;
+                else if (mp[a[i][j]] == 2)
+                    p[i] += 1;
+            }
+        }
+
+        cout << p[0] << " " << p[1] << " " << p[2] << endl;
+    }
+
+    return 0;
+}
