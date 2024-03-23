85d899ee0f5e389460fb3250f3dce8d87ab17044
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-03-23 12:13:00 +0600

    cp

---
+++ Week-02/Day-02/E_Plus_or_Minus.cpp
@@ -0,0 +1,27 @@
+/******************************
+ * author: Md Rakibul Hasan
+ * date: 2024-03-23 12:04:10
+ ******************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c)
+            cout << "+" << '\n';
+        else
+            cout << "-" << '\n';
+    }
+
+    return 0;
+}
