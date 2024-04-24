abade318fb36aad0fd44f994c81986f0a3cd456a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-04-24 13:43:12 +0600

    cp

---
+++ Week-05/Day-06/P_BerSU_Ball.cpp
@@ -0,0 +1,46 @@
+/******************************
+ * author: Md Rakibul Hasan
+ * date: 2024-04-24 13:12:05
+ ******************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void Solve()
+{
+    int n, m;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = -1; ++i < n;)
+        cin >> v[i];
+
+    cin >> m;
+    vector<int> v1(m);
+    for (int i = -1; ++i < m;)
+        cin >> v1[i];
+
+    sort(v.begin(), v.end());
+    sort(v1.begin(), v1.end());
+
+    ll c = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (abs(v[i] - v1[j]) <= 1)
+                c++, v1[j] = INT_MAX, v[i] = INT_MAX;
+
+    cout << c;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        Solve();
+        cout << '\n';
+    }
+}
