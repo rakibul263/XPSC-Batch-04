493a294df942821cef0bd1f20f7c06aabd33b1ce
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-05-14 19:27:00 +0600

    cp

---
+++ Week-08/Day-03/A_Maximum_GCD.cpp
@@ -0,0 +1,26 @@
+/******************************
+ * author: Md Rakibul Hasan
+ * date: 2024-05-14 19:24:32
+ ******************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
+
+    return 0;
+}
