b1dfb695a415b3969a891ea0914704b5e2e1855c
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-05-26 22:56:42 +0600

    cp

---
+++ Weel-10/Day-02/Graph_Cost.cpp
@@ -0,0 +1,68 @@
+/******************************
+* author: Md Rakibul Hasan
+* date: 2024-05-26 22:54:44
+******************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        stack<int> value;
+        stack<int> index;
+        long long total_cost = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            while (value.size() > 1 && value.top() > a[i])
+            {
+                int cur = value.top();
+                value.pop();
+                if (cur >= value.top())
+                {
+                    index.pop();
+                }
+                else
+                {
+                    value.push(cur);
+                    break;
+                }
+            }
+            value.push(a[i]);
+            index.push(i);
+        }
+
+        int last_item = value.top();
+        value.pop();
+        int last_index = index.top();
+        index.pop();
+
+        while (!index.empty())
+        {
+            total_cost += max(last_item, value.top()) * (last_index - index.top());
+            last_item = value.top();
+            value.pop();
+            last_index = index.top();
+            index.pop();
+        }
+
+        cout << total_cost << "\n";
+    }
+
+    return 0;
+}
